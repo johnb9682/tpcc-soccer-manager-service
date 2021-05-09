1fa35a1e788728f935c1c646b90c3c0bb9c72deb
John <johnb9682@gmail.com>
2021-05-09 15:06:12 +0800

    initial commit

---
+++ src/main/java/com/tpcc/soccer/manager/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.tpcc.soccer.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author John
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public String helloWorld() throws InterruptedException {
+
+
+        return "Hello World";
+    }
+}
