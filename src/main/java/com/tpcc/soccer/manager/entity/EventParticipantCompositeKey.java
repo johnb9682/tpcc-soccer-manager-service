d48fa036ca8e016094be718d9f169265091f6d2b
John <johnb9682@gmail.com>
2021-07-06 22:20:11 +0800

    Fix Serilization problem

--- src/main/java/com/tpcc/soccer/manager/entity/EventParticipantCompositeKey.java
+++ src/main/java/com/tpcc/soccer/manager/entity/EventParticipantCompositeKey.java
@@ -2,8 +2,10 @@ package com.tpcc.soccer.manager.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class EventParticipantCompositeKey {
+public class EventParticipantCompositeKey implements Serializable {
     private Integer eventParticipantId;
     private Integer userId;
     private Integer eventId;
--- src/main/java/com/tpcc/soccer/manager/entity/TeamMemberCompositeKey.java
+++ src/main/java/com/tpcc/soccer/manager/entity/TeamMemberCompositeKey.java
@@ -2,8 +2,10 @@ package com.tpcc.soccer.manager.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class TeamMemberCompositeKey {
+public class TeamMemberCompositeKey implements Serializable {
     private Integer teamMemberId;
     private Integer userId;
     private Integer teamId;
