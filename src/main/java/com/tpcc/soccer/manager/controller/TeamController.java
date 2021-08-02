3a0ae72e2c5570e7c81e616305216d5180e1d556
Liu Jikun <45059360+JaykingLJK@users.noreply.github.com>
2021-08-02 21:08:24 +0800

    woshishabi

--- src/main/java/com/tpcc/soccer/manager/controller/TeamController.java
+++ src/main/java/com/tpcc/soccer/manager/controller/TeamController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class TeamController {
+    @Autowired
     private TeamService teamService;
 
     @CrossOrigin
