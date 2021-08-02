2d7dc7df03d81573f10da9cc41e360e42e50279e
Liu Jikun <45059360+JaykingLJK@users.noreply.github.com>
2021-08-02 21:13:24 +0800

    woshishabi

--- src/main/java/com/tpcc/soccer/manager/controller/TeamController.java
+++ src/main/java/com/tpcc/soccer/manager/controller/TeamController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
-
+@RestController
 public class TeamController {
+    @Autowired
     private TeamService teamService;
 
     @CrossOrigin
