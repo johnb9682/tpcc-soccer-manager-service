9995ce327b0da74db3e28f021f73c89d42f1d335
John <johnb9682@gmail.com>
2021-08-28 15:09:02 +0800

    Add lambda event handler

--- src/main/java/com/tpcc/soccer/manager/StreamLambdaHandler.java
+++ src/main/java/com/tpcc/soccer/manager/StreamLambdaHandler.java
@@ -9,10 +9,13 @@ import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.time.Instant;
+import java.util.EnumSet;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
@@ -22,18 +25,18 @@ public class StreamLambdaHandler implements RequestStreamHandler {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
 
             // For applications that take longer than 10 seconds to start, use the async builder:
-            long startTime = Instant.now().toEpochMilli();
-            handler = new SpringBootProxyHandlerBuilder()
-                    .defaultProxy()
-                    .asyncInit(startTime)
-                    .springBootApplication(Application.class)
-                    .buildAndInitialize();
+//            long startTime = Instant.now().toEpochMilli();
+//            handler = new SpringBootProxyHandlerBuilder()
+//                    .defaultProxy()
+//                    .asyncInit(startTime)
+//                    .springBootApplication(Application.class)
+//                    .buildAndInitialize();
 
             // we use the onStartup method of the handler to register our custom filter
-//            handler.onStartup(servletContext -> {
-//                FilterRegistration.Dynamic registration = servletContext.addFilter("CognitoIdentityFilter", CognitoIdentityFilter.class);
+            handler.onStartup(servletContext -> {
+//                FilterRegistration.Dynamic registration = servletContext.;
 //                registration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
-//            });
+            });
         } catch (ContainerInitializationException e) {
             // if we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
