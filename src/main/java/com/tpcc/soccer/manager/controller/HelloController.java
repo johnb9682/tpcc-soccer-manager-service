f565c10ec704253c3de9a7e68649795ee5a1f172
johnb9682 <johnb9682@gmail.com>
2021-09-04 11:49:55 +0800

    Change API Response

--- src/main/java/com/tpcc/soccer/manager/controller/HelloController.java
+++ src/main/java/com/tpcc/soccer/manager/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public String helloWorld() throws InterruptedException {
 
-        return "TPCC Soccer Manager Test";
+        return "TPCC Soccer Manager Test API";
     }
 }
