5e133b4ed95badc83673b406d083aee8322adc0a
Liu Jikun <45059360+JaykingLJK@users.noreply.github.com>
2021-09-18 22:49:42 +0800

    invitation request no create time no id

--- src/main/java/com/tpcc/soccer/manager/dto/InvitationEventRequest.java
+++ src/main/java/com/tpcc/soccer/manager/dto/InvitationEventRequest.java
@@ -8,11 +8,9 @@ import java.sql.Timestamp;
 
 @Data
 public class InvitationEventRequest {
-    private int invitationId;
     private int eventId;
     private int senderId;
     private int receiverId;
     private int status; // 0 is pending; -1 is rejected; 1 is accepted;
-    private int createTime;
     private int responseTime;
 }
